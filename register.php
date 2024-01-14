ac3e15c74ba49bfe6f1fc3176e9233ab0a978fac
maudyaulia <rizkymaudy16@gmail.com>
2024-01-14 13:36:03 +0700

    first commit

---
+++ register.php
@@ -0,0 +1,16 @@
+<?php
+require 'koneksi.php';
+$fullname = $_POST["fullname"];
+$username = $_POST["username"];
+$email = $_POST["email"];
+$password = $_POST["password"];
+
+$query_sql = "INSERT INTO tbuser (fullname, username, email, password)
+VALUE ('$fullname', '$username', '$email', '$password')";
+
+if (mysqli_query($conn, $query_sql)) {
+    header("Location: login.html");
+}else {
+    echo "Pendaftaran Gagal : " . mysqli_error($conn);
+}
+?>
